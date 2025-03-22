11e4167e143ffb121da8d1803411b23f8a7ec3c2
hassan5544 <hmh.lol55@gmail.com>
2025-03-22 15:49:48 +0300

    commit by hassan add migrations add tests

---
+++ Infrastructure/Migrations/20250321165419_AddIndexStatus.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Tasks");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_task_status",
+                table: "Tasks",
+                column: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_task_status",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Tasks",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
